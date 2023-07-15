b91181f396a56d25d57a675701ae7ecce9fe0dbf
Fening <feningmanuel@gmail.com>
2023-07-15 03:16:49 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	m = 'u';
+	while
+		(m >= 'b') {
+			putchar(m);
+			m--;
+		}
+	putchar('\n');
+	return (0);
+}
