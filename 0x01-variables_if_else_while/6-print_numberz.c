36b71c80a1b92d438758b05b0742af4caa30939a
Fening <feningmanuel@gmail.com>
2023-07-15 03:10:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int p;
+
+	p = 0;
+	while
+		(p < 10) {
+			putchar(p + '0');
+			p++;
+		}
+	putchar('\n');
+	return (0);
+}
