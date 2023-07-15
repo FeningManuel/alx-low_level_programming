4a90c8fca9c08a8be84716ba68148c2b895db62c
Fening <feningmanuel@gmail.com>
2023-07-15 03:29:12 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	int d;
+
+	m = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(m <= 'f') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
